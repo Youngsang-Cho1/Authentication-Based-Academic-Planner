603d3d9f2440a9a88bcc2b461d00a93e907befa3
Youngsang-Cho1 <youngsang1211@gmail.com>
2025-04-23 01:33:35 -0400

    fixing error

--- src/app.mjs
+++ src/app.mjs
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import tasksRoutes from "./routes/tasks.mjs";
 import authRoutes from "./routes/auth.mjs";
 import { connectToDB } from "./db.mjs";
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,7 +24,7 @@ app.use(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "src", "views"));
 
-app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", authRoutes);
